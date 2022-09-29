355f72a81422e7aa5cb2372eee9995fcf5ce0d00
Bhattacharyya-Avi <avindra.kodeeo@gmail.com>
2022-09-29 15:31:25 +0600

    Client site validation

--- resources/views/backend/product/form.blade.php
+++ resources/views/backend/product/form.blade.php
@@ -6,22 +6,22 @@
   <div class="form-group">
     <label for="exampleInputPassword1">Name</label>
     <input name="pro_name" type="text" class="form-control" id="exampleInputPassword1" 
-    placeholder="">
+    placeholder="" required>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Quantity</label>
     <input name="pro_quantity" type="number" class="form-control" id="exampleInputPassword1" 
-    placeholder="">
+    placeholder=""required min="1" max="10">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Price</label>
     <input name="pro_price" type="number" class="form-control" id="exampleInputPassword1" 
-    placeholder="">
+    placeholder=""required>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Weight</label>
     <input name="pro_weight" type="number" class="form-control" id="exampleInputPassword1" 
-    placeholder="">
+    placeholder=""required>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Description</label>
@@ -35,7 +35,7 @@
   </div>
   <div class="form-group">
     <label for="exampleFormControlSelect2">Select Category</label>
-    <select name="category_name"  class="form-control" id="exampleFormControlSelect2">
+    <select name="category_name"  class="form-control" id="exampleFormControlSelect2" required>
       
     @foreach($categories as $data)
     <option value="{{$data->name}}">{{$data->name}}</option>
