e28416471435448dc22c8ff2c56de5ed410415ec
Bhattacharyya-Avi <avindra.kodeeo@gmail.com>
2022-11-08 15:13:15 +0600

    view share and show category & product name

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+
 
 class HomeController extends Controller
 {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\Facades\View;
+Use App\Models\Category;
+use App\Models\Product;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,5 +27,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        $categories=Category::all();
+        View::share('categories', $categories);
+
+        $products=Product::all();
+        View::share('products',$products);
+        //view::share('key', variable) same name hobe
     }
 }
